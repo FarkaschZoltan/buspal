c8fd122eb4772a3e395e1e28e8dec52f76cd6076
FarkaschZoltan <69429733+FarkaschZoltan@users.noreply.github.com>
2020-10-06 14:55:06 +0200

    Update BusTrip.java

--- hu/Farkasch/buspalbackend/objects/BusTrip.java
+++ hu/Farkasch/buspalbackend/objects/BusTrip.java
@@ -2,6 +2,43 @@ package hu.Farkasch.buspalbackend.objects;
 
 import java.utils.ArrayList;
 
-public class BusTrip{ //to-do
+public class BusTrip{
     private ArrayList<BusStop> busStopList;
+    private int tripId;
+
+    public BusTrip(){
+        busStopList = new ArrayList<BusStop>();
+        tripId = -1; //no trip id
+    }
+
+    public BusTrip(int tripId, ArrayList<BusStop> busStopList){
+        this.tripId = tripId;
+        this.busStopList = busStopList;
+    }
+
+    //basic getters and setters
+
+    public ArrayList<BusStop> getBusStopList(){
+        return busStopList;
+    }    
+
+    public int getTripId(){
+        return tripId;
+    }
+
+    public void setBusStopList(ArrayList<BusStop> busStopList){
+        this.busStopList = busStopList;
+    }
+
+    public void setTripId(int tripId){
+        this.tripId = tripId;
+    }
+
+    //would be usefull to organize busStopList in ascending order (stopSequencePlace)
+
+    public void addBusStop(BusStop bs){
+        busStopList.add(bs);
+    }
+
+    public 
 }
