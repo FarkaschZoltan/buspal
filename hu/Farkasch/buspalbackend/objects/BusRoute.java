a626ca6d8c8d0c7aa6e4766256faff2e8c1ff432
FarkaschZoltan <69429733+FarkaschZoltan@users.noreply.github.com>
2020-10-06 14:54:54 +0200

    Create BusRoute.java

---
+++ hu/Farkasch/buspalbackend/objects/BusRoute.java
@@ -0,0 +1,8 @@
+package hu.Farkasch.buspalbackend.objects;
+
+import hu.Farkasch.buspalbackend.datastructures.Coordinates;
+
+public class BusRoute{
+    private ArrayList<BusTrip> busStripList;
+    private int routeId;
+}
