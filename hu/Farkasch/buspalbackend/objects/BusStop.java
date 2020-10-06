229978346887a4a89d18496958f9fb2423e11c4d
FarkaschZoltan <69429733+FarkaschZoltan@users.noreply.github.com>
2020-10-06 11:18:59 +0200

    Expanding BusStop.java and creating BusTrip.java
    Expanding on previous code on BusStop. java ()adding additional values, getters and setters)
    
    Creating a VERY basic BusTrip.java

--- hu/Farkasch/buspalbackend/objects/BusStop.java
+++ hu/Farkasch/buspalbackend/objects/BusStop.java
@@ -1,22 +1,32 @@
 package hu.Farkasch.buspalbackend.objects;
 
 import hu.Farkasch.buspalbackend.datastructures.Coordinates;
+import java.time.LocalTime;
 
 public class BusStop {
     private Coordinates cords;
     private String stopName;
     private int stopId;
+    private LocalTime arrivalTime;
+    private LocalTime departureTime;
+    private int stopSequencePlace;
 
     public BusStop(){
-        cords = new Coordinates();
-        stopName = "";
-        stopId = -1; //no id = -1
+        this.cords = new Coordinates();
+        this.stopName = "";
+        this.stopId = -1; //no id = -1
+        this.arrivalTime = arrivalTime.of(0, 0, 0);
+        this.arrivalTime = arrivalTime.of(0, 0, 0);
+        this.stopSequencePlace = -1; //not in a sequence
     }
 
-    public BusStop(int stopId, String stopName, Coordinates cords){
+    public BusStop(int stopId, String stopName, Coordinates cords, String timeDataArrival, String timeDataDeparture, int stopSequencePlace){
         this.stopId = stopId;
         this.stopName = stopName;
         this.cords = cords;
+        arrivalTime = arrivalTime.parse(timeDataArrival);
+        departureTime = departureTime.parse(timeDataDeparture);
+        this.stopSequencePlace = stopSequencePlace;
     }
 
     //basic getters and setters
@@ -33,20 +43,52 @@ public class BusStop {
         return stopId;
     }
 
+    public LocalTime getArrivalTime(){
+        return arrivalTime;
+    }
+
+    public LocalTime getDepartureTime(){
+        return departureTime;
+    }
+
+    public int getStopSequencePlace(){
+        return getStopSequencePlace;
+    }
+
     public void setCords(Coordinates cords){
         this.cords = cords;
     }
 
     public void setStopName(String stopName){
-        this.stopName= stopName;
+        this.stopName = stopName;
     }
 
     public void setStopId(int stopId){
         this.stopId = stopId;
     }
 
+    public void setArrivalTime(String timeDataArrival){ //String version
+        arrivalTime = arrivalTime.parse(timeDataArrival);
+    }
+
+    public void setArrivalTime(LocalTime arrivalTime){ //LocalTime version
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setDepartureTime(String timeDataDeparture){ //String version
+        departureTime = departureTime.parse(timeDataDeparture);
+    }
+
+    public void setDepartureTime(LocalTime departureTime){ //LocalTime version
+        this.departureTime = departureTime;
+    }
+
+    public void setStopSequencePlace(int stopSequencePlace){
+        this.stopSequencePlace = stopSequencePlace;
+    }
+
     @Override
-    public String toString(){
+    public String toString(){ //not finished
         return String.format("id: " + stopId + ", name: " + stopName + ", coordinates: " + cords);
     }
 }
---
+++ hu/Farkasch/buspalbackend/objects/BusTrip.java
@@ -0,0 +1,7 @@
+package hu.Farkasch.buspalbackend.objects;
+
+import java.utils.ArrayList;
+
+public class BusTrip{
+    private ArrayList<BusStop> busStopList;
+}
