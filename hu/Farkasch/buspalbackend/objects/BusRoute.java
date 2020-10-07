7f35891e910431efa7c75da57f39dd4f05eaa34c
FarkaschZoltan <y2smar@inf.elte.hu>
2020-10-07 14:35:10 +0200

    Update BusRoute.java
    Created the "skeleton" of the generate function

--- hu/Farkasch/buspalbackend/objects/BusRoute.java
+++ hu/Farkasch/buspalbackend/objects/BusRoute.java
@@ -3,6 +3,15 @@ package hu.farkasch.buspalbackend.objects;
 import hu.Farkasch.buspalbackend.datastructures.Coordinates;
 
 public class BusRoute{
-    private ArrayList<BusTrip> busStripList;
+    private ArrayList<BusTrip> busTripList;
     private int routeId;
+    private boolean favourite = false;
+
+    public BusRoute (int routeId){
+        generate(routeId);
+    }
+
+    public void generate(int routeId){ //to-do: renerating the needed BusTrips and BusStops based on the given routeId
+
+    }
 }
