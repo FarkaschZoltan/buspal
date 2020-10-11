236f9d44f0c9697ad73a23efbdda4b2143ffba54
FarkaschZoltan <y2smar@inf.elte.hu>
2020-10-11 18:53:11 +0200

    Update BusRoute.java
    Fucking around, and getting nothing done

--- hu/Farkasch/buspalbackend/objects/BusRoute.java
+++ hu/Farkasch/buspalbackend/objects/BusRoute.java
@@ -1,18 +1,18 @@
 package hu.farkasch.buspalbackend.objects;
 
-import hu.farkasch.buspalbackend.datastructures.Coordinates;
+import hu.thepocok.statements.Statements;
 import java.util.ArrayList;
 
-public class BusRoute{
+public class BusRoute {
     private ArrayList<BusTrip> busTripList;
     private int routeId;
     private boolean favourite = false;
 
-    public BusRoute (int routeId){
+    public BusRoute(int routeId){
         generate(routeId);
     }
 
-    public void generate(int routeId){ //to-do: renerating the needed BusTrips and BusStops based on the given routeId
-
+    public void generate(int routeId){ // to-do: generating the needed BusTrips and BusStops based
+        //fuck jdbc
     }
 }
