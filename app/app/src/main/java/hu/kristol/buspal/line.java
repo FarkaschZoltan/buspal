b08057e9347c1981227efd1334d61eaf17ca9b23
kristol <dyy0e5@inf.elte.hu>
2020-10-12 14:55:37 +0200

    UI design - activity_line update

---
+++ app/app/src/main/java/hu/kristol/buspal/line.java
@@ -0,0 +1,14 @@
+package hu.kristol.buspal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class line extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line);
+    }
+}
