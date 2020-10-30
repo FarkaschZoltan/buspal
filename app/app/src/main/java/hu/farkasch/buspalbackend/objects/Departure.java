629b872283b9e6b23393e43679d715a2f2df900f
ThePocok <yvhlr3@inf.elte.hu>
2020-10-30 13:55:54 +0100

    Typos corrected

--- app/app/src/main/java/hu/farkasch/buspalbackend/objects/Departure.java
+++ app/app/src/main/java/hu/farkasch/buspalbackend/objects/Departure.java
@@ -1,29 +1,31 @@
 package hu.farkasch.buspalbackend.objects;
 
+import java.io.Serializable;
+
 import hu.farkasch.buspalbackend.datastructures.Time;
 
-public class Departure { //everything is public for easier access
-    public int routeId;
+public class Departure implements Serializable { //everything is public for easier access
+    public int tripId;
     public String routeName;
     public String destination;
     public Time departureTime;
 
     public Departure(){
-        routeId = -1;
+        tripId = -1;
         routeName = "";
         destination = "";
         departureTime = new Time();
     }
 
-    public Departure(int routeId, String routeName, String destination, Time departureTime){
-        this.routeId = routeId;
+    public Departure(int tripId, String routeName, String destination, Time departureTime){
+        this.tripId = tripId;
         this.routeName = routeName;
-        this. destination = destination;
+        this.destination = destination;
         this.departureTime = departureTime;
     }
 
     @Override
     public String toString(){
-        return String.format("route id: " + routeId + ", route name: " + routeName + ", destination: " + destination + ", departure time: " + departureTime );
+        return String.format("trip id: " + tripId + ", route name: " + routeName + ", destination: " + destination + ", departure time: " + departureTime );
     }
 }
