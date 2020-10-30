c279698873700641b75366fa119a9746f6508308
FarkaschZoltan <y2smar@inf.elte.hu>
2020-10-30 16:33:36 +0100

    Create StopDepartureInterface.java

---
+++ app/app/src/main/java/hu/thepocok/interfaces/StopDepartureInterface.java
@@ -0,0 +1,21 @@
+package hu.thepocok.interfaces;
+
+import java.util.List;
+
+import hu.farkasch.buspalbackend.objects.Departure;
+
+import retrofit2.Call;
+
+import retrofit2.http.GET;
+
+import retrofit2.http.Query;
+
+public interface StopDepartureInterface {
+
+    public static String BASE_URL = "http://80.98.90.176:9876/";
+
+    @GET("index.php?")
+
+    Call<List<Departure>> departureList(@Query("username") String username, @Query("password") String password, @Query("host") String host, @Query("database") String db_name, @Query("statement") String statement);
+
+}
